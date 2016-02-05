cd72088fc18e63c0db10b84fa9479bd48ebb5d16
yhikishima <lallapalooza1120@gmail.com>
2016-02-05 09:03:15 +0900

    adjust react component

--- electron-react-redux/index.js
+++ electron-react-redux/index.js
@@ -15,3 +15,28 @@ app.on("window-all-closed", e => {
 ipcMain.on("click-my-button", (sender, e) => {
   console.log(e);
 });
+
+const count = (state, action) => {
+  state = state || 0;
+  switch(action.type) {
+    case "COUNT_UP":
+      return state + 1;
+  }
+  return state;
+}
+
+// const myStore = redux.createStore((state, action) => {
+//   state = state || {};
+//   return {count: count(state.count, action)};
+// });
+
+// const render = () => {
+//   mainWindow.webContents.send("render", myStore.getState())
+// };
+
+// ipcMain.on("dispatch-store", (sender, e) => {
+//   myStore.dispatch(e);
+//   render();
+// });
+
+// mainWindow.webContents.on("dom-ready", render); // ← DOMの準備ができたら一度レンダリング
--- electron-react-redux/my-app.jsx
+++ electron-react-redux/my-app.jsx
@@ -3,11 +3,17 @@
 const React = require("react");
 
 class MyApp extends React.Component {
+  _onClick() {
+    ipcRenderer.send("dispatch-store", {
+      type: "COUNT_UP"
+    });
+  }
+
   render() {
-    return (
-      React.createElement('div', {className: "commentBox"},
-        "Hello, world! I am a CommentBox."
-      )
-    );
+    return <div>
+      <h1>React in Electron!</h1>
+      <p>{this.props.count}</p>
+      <button onClick={this._onClick}>Click</button>
+    </div>;
   }
 }
